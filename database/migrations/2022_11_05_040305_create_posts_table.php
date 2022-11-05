61480788927ec0046bf5653c0961c4b38975e5a4
hugoruiz00 <hugo.ruiz.pz@gmail.com>
2022-11-04 23:05:43 -0600

    Adding post and post status migration and model, and view is updated

--- database/migrations/2022_11_05_040305_create_posts_table.php
+++ database/migrations/2022_11_05_040305_create_posts_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->integer('reputation')->default(1);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 150);
+            $table->text('body');
+            $table->integer('votes');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_status_id')->constrained();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
