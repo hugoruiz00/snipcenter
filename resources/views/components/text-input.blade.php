383a3416bbf8e2ffd935377aa36f6356ea1839df
hugoruiz00 <hugo.ruiz.pz@gmail.com>
2022-11-09 21:14:38 -0600

    Updating autofill color of input and reveal password icon color

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm bg-[#171D2E] border-[#1C6FAC] border-2 focus:border-[#1C6FAC] focus:ring focus:ring-[#1C6FAC] focus:ring-opacity-50']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm bg-[#171D2E] border-[#1C6FAC] border-2 focus:border-[#1C6FAC] focus:ring focus:ring-[#1C6FAC] focus:ring-opacity-50 text-white focus:bg-[#212940] color-white']) !!}>
