427b9c36b8566832c9b7ed0e55efe73c6440bb91
hugoruiz00 <hugo.ruiz.pz@gmail.com>
2022-11-13 21:11:18 -0600

    Updating views Welcome view file is renamed Deleting header section Updating styles when user is logged in Updating styles for loggout button and user name in navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,7 +23,7 @@
                 @auth
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
-                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            <button class="flex items-center text-sm font-medium text-white hover:text-[#1C6FAC] focus:outline-none focus:text-[#1C6FAC] transition duration-150 ease-in-out">
                                 <div>{{ Auth::user()->name }}</div>
 
                                 <div class="ml-1">
@@ -81,8 +81,8 @@
         @auth
             <div class="pt-4 pb-1 border-t border-gray-200">
                 <div class="px-4">
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    <div class="font-medium text-base text-white">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-[#ACACAC]">{{ Auth::user()->email }}</div>
                 </div>
 
                 <div class="mt-3 space-y-1">
